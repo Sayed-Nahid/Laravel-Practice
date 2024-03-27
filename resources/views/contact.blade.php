02f12486fb5036418eaff0f34bfd4a923c0fe864
Sayed Nahid <nahid15-3849@diu.edu.bd>
2024-03-27 23:55:13 +0600

    adding Home contact about page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+@push('title')
+    <title>Contact</title>
+@endpush
+@section('main-section')
+<h1 class="text-center"> Contact Page</h1>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,3 @@
+@include('layouts.header')
+<div class="container">@yield('main-section')</div>
+@include('layouts.footer')
